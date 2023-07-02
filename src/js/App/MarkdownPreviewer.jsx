9ea014c74c7911daf1e98d21943ed0d62b15e4a5
Adam Fijałkowski <fijalkowski.adam@outlook.com>
2023-07-03 00:49:50 +0200

    kaszana

--- src/js/App/MarkdownPreviewer.jsx
+++ src/js/App/MarkdownPreviewer.jsx
@@ -5,15 +5,17 @@ import parse from "html-react-parser";
 
 export default function QuoteMachine() {
   const [currentValue, setCurrentValue] = useState("");
-  fetch("js/data/defaultText.txt")
-    .then((response) => response.text())
-    .then((data) => {
-      const textString = data;
-      setCurrentValue(textString);
-    })
-    .catch((error) => {
-      console.error("Błąd podczas wczytywania pliku:", error);
-    });
+  useEffect(() => {
+    fetch("./data/defaultText.txt")
+      .then((response) => response.text())
+      .then((data) => {
+        const textString = data;
+        setCurrentValue(textString);
+      })
+      .catch((error) => {
+        console.error("Błąd podczas wczytywania pliku:", error);
+      });
+  }, []);
 
   const handleClick = (event) => {
     const container = document.getElementById("markdown-previever");
@@ -22,25 +24,25 @@ export default function QuoteMachine() {
 
     Array.from(container.querySelectorAll(`:scope > *`))
       .filter((child) => {
-        console.log(child.querySelector(`#${event.target.id}`));
         return !child.querySelector(`#${event.target.id}`);
+      })
+      .filter((child) => {
+        console.log(child.classList.contains("divider"));
+        return !child.classList.contains("divider");
       })
       .forEach((child) => child.classList.toggle("d-none"));
     Array.from(container.querySelectorAll(`:scope > *`))
       .filter((child) => {
-        console.log(child.querySelector(`#${event.target.id}`));
         return child.querySelector(`#editor`);
       })
-      .forEach((child) => child.classList.toggle("h-100"));
+      .forEach((child) => child.classList.toggle("h-100-custom"));
     Array.from(container.querySelectorAll(`:scope > *`))
       .filter((child) => {
-        console.log(child.querySelector(`#${event.target.id}`));
         return child.querySelector(`#preview`);
       })
       .forEach((child) => child.classList.toggle("min-h-100"));
     Array.from(container.querySelectorAll(`:scope > *`))
       .filter((child) => {
-        console.log(child.querySelector(`#${event.target.id}`));
         return child.querySelector(`#editor`);
       })
       .forEach((child) =>
@@ -96,6 +98,7 @@ export default function QuoteMachine() {
           <div id="preview">{parse(marked(currentValue, options))}</div>
         </div>
       </div>
+      <div className="divider"></div>
     </>
   );
 }
